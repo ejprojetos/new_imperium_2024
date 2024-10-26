9e62080bf143724ff019132455b8166d0929227d
jeffersonb7 <jeffersonbfs07@gmail.com>
2024-10-26 14:34:24 -0300

    feat: criação dos usuários e definição do modelo

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,34 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from .views import ClinicAdminViewSet
-
-
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import (
-#     PatientViewSet,
-#     DoctorViewSet,
-#     ReceptionistViewSet,
-#     ClinicAdminViewSet
-# )
-
-router = DefaultRouter()
-router.register(r'patients', ClinicAdminViewSet)
-# router.register(r'doctors', DoctorViewSet)
-# router.register(r'receptionists', ReceptionistViewSet)
-# router.register(r'clinic-admins', ClinicAdminViewSet)
-
+from django.urls import path
+from .views import AdminClinicRegisterView, DoctorRegisterView, PacientRegisterView, RecepcionistRegisterView
 
 urlpatterns = [
-    # path('users/admin-clinic', ClinicAdminViewSet.as_view(), name='create-admin-clinic'),
-    path('', include(router.urls)),  # Use the router's URLs
-
+    path('register/admin-clinic/', AdminClinicRegisterView.as_view(), name='register_admin_clinic'),
+    path('register/doctor/', DoctorRegisterView.as_view(), name='register_doctor'),
+    path('register/pacient/', PacientRegisterView.as_view(), name='register_pacient'),
+    path('register/recepcionist/', RecepcionistRegisterView.as_view(), name='register_recepcionist'),
 ]
-
-# urlpatterns = [
-#     path('users/', UserView.as_view()),
-#     path('patients', PatientViewSet.as_view()),
-#     path('doctors', DoctorViewSet.as_view()),
-#     path('', include(router.urls)),  # Use the router's URLs
-# ]
