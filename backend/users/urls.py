da1647774a75e3fe47814127ddd237edc0c6b147
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-10-26 17:57:24 -0300

    fix: conflit

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views import ClinicAdminViewSet
-from patient.views import MedicalFileViewSet
-
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import (
-#     PatientViewSet,
-#     DoctorViewSet,
-#     ReceptionistViewSet,
-#     ClinicAdminViewSet
-# )
-
-router = DefaultRouter()
-router.register(r'patients', ClinicAdminViewSet)
-router.register(r'medicalfile',MedicalFileViewSet)
-# router.register(r'doctors', DoctorViewSet)
-# router.register(r'receptionists', ReceptionistViewSet)
-# router.register(r'clinic-admins', ClinicAdminViewSet)
-
-
-urlpatterns = [
-    # path('users/admin-clinic', ClinicAdminViewSet.as_view(), name='create-admin-clinic'),
-    path('', include(router.urls)),  # Use the router's URLs
-]
-
-# urlpatterns = [
-#     path('users/', UserView.as_view()),
-#     path('patients', PatientViewSet.as_view()),
-#     path('doctors', DoctorViewSet.as_view()),
-#     path('', include(router.urls)),  # Use the router's URLs
-# ]
-=======
 from django.urls import path
 from .views import AdminClinicRegisterView, DoctorRegisterView, PacientRegisterView, RecepcionistRegisterView
 
@@ -44,4 +7,3 @@ urlpatterns = [
     path('register/pacient/', PacientRegisterView.as_view(), name='register_pacient'),
     path('register/recepcionist/', RecepcionistRegisterView.as_view(), name='register_recepcionist'),
 ]
->>>>>>> main
