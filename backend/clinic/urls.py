80522c1bab2f1e72fe9d71d48d32a9d6b4f5eff5
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-11-13 21:55:36 -0300

    fix: ajustando as rotas do medical-records

--- backend/clinic/urls.py
+++ backend/clinic/urls.py
@@ -2,8 +2,9 @@ from django.urls import path, include
 from .views import RoomViewSet, MedicalRecordViewSet, NotificationViewSet
 from rest_framework.routers import DefaultRouter
 
-
 urlpatterns = [
+    path('medical-records/<int:appointment_id>/', MedicalRecordViewSet.as_view({'get': 'list'})),
+    path('medical-records/<int:pk>/', MedicalRecordViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('notifications/user/<int:user_id>', NotificationViewSet.as_view({'get': 'list'})),
     path('notifications/<int:pk>/', NotificationViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
     path('<int:clinic_id>/rooms/', RoomViewSet.as_view({'get': 'list', 'post': 'create'})),
