37dfb78cdef5061601d64946823f7267c17df352
CarlosG18 <c.gabriel.abc18@gmail.com>
2024-12-03 10:18:44 -0300

    remove: removendo o endpoint de envio de email

--- backend/imperium/urls.py
+++ backend/imperium/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from commom.views import EmailAPIView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/clinics/', include('clinic.urls')),
-    path('api/send-email/', EmailAPIView.as_view(), name='send-email'), 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
