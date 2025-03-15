94c0ab4b56f6488ba95ebe4854e93f02de82b36a
Josmar Junior <josmartrigueiro.jr@gmail.com>
2025-03-15 11:00:08 -0300

    fix: adjust validation and implement avatar letters

--- front/src/lib/utils.ts
+++ front/src/lib/utils.ts
@@ -19,3 +19,10 @@ export function areArraysEqual(arr1: any[] | undefined, arr2: any[]): boolean {
         arr1.every((value) => arr2.includes(value)) && arr2.every((value) => arr1.includes(value))
     )
 }
+
+export function getInitials(name: string) {
+    const parts = name.trim().split(/\s+/) // Remove espaços extras e divide por espaço
+    if (parts.length === 1) return parts[0][0].toUpperCase() // Se for um nome só
+
+    return (parts[0][0] + parts[parts.length - 1][0]).toUpperCase()
+}
